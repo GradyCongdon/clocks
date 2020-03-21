d4c4f1abc2a26c612a1953e0f4ad593728417a8b
GradyCongdon <gradycongdon@gmail.com>
2020-03-21 17:33:18 -0400

    github pages

---
+++ docs/precache-manifest.959f40c7ab1ec0830e2d387b08129cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099a3f988f326156f6b2f19af95e23d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dabf43ef876412ae509",
+    "url": "/static/css/main.06e033b7.chunk.css"
+  },
+  {
+    "revision": "ba71407639144ef71c81",
+    "url": "/static/js/2.017dbb06.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.017dbb06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dabf43ef876412ae509",
+    "url": "/static/js/main.5ba8d992.chunk.js"
+  },
+  {
+    "revision": "0d71fe5de13e153a367e",
+    "url": "/static/js/runtime-main.292d49e8.js"
+  }
+]);
